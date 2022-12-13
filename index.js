ce9944820bbadb514b86fa57227b5943f139ed2c
CKraft11 <cadenkraft@gmail.com>
2022-12-12 22:55:41 -0600

    added headers

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ app.get("/api", async (req, res) => {
           itemsArr[i] = {itemName:itemsArr[i], quantity:quantity};
     }
     surplus.items = itemsArr;
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+    res.setHeader("Content-Type", "application/json");
     res.send(surplus);
   } catch (err) {
     console.error(err);
